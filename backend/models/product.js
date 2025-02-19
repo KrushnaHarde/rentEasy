4ffd9855807a489cdf4e122412f6348a44bcb2b5
Krushna <hardekrushna88@gmail.com>
2025-02-19 22:46:14 +0530

    Schema added

---
+++ backend/models/product.js
@@ -0,0 +1,60 @@
+// const {createHmac, randomBytes} = require('crypto');
+const { Schema, model } = require("mongoose");
+// const { createTokenForUser,validateToken } = require('../services/authentication');
+
+const productSchema = new Schema(
+  {
+    uploadedBy:{ 
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+        index: true
+    },
+    name: { 
+        type: String,
+        required: true
+    },
+    category: { 
+        type: String, 
+        default: general
+    },
+    description: { 
+        type: String, 
+        required: true 
+    },
+    productImage: { 
+        type: String
+    },
+    productCondition: { 
+        type: String, 
+        required: true
+    },
+    review:{
+        type: String
+    },
+    rating:{
+        type: Number, 
+        min:1,
+        max:5,
+        default: 0  
+    },
+    price: { 
+        type: Number, 
+        required: true 
+    },
+    duration: {
+        days: {
+            // this is the number of hours 
+            type: Number,
+            default: 0,
+            min: 0
+        },
+    }   
+    
+  },
+  { timestamps: true }
+);
+
+const Product = model("Product", productSchema);
+module.exports = Product;
+ 
