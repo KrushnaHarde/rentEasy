993cab3597f137a3b8bef72b83915b7f9f860858
Krushna <hardekrushna88@gmail.com>
2025-07-01 12:19:51 +0530

    resolved error of PATCH request - approve rental

--- backend/index.js
+++ backend/index.js
@@ -45,7 +45,7 @@ const corsOptions = {
     "https://admin.socket.io"
   ],
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
+  methods: ["GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"]
 };
 
 app.use(cors(corsOptions));
